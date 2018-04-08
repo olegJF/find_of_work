60699d02bf8bc666eb923aebeab4aa69d35db1ab
oleg <jf2@ua.fm>
2018-04-08 12:12:40 +0300

    DB settings

--- vacancy/settings.py
+++ vacancy/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES = ['default'].update(db_from_env)
