615028ded9ed03cabd0521316f95b73b3d585c4d
oleg <jf2@ua.fm>
2018-04-08 12:55:37 +0300

    change get method

--- scraping/views.py
+++ scraping/views.py
@@ -7,93 +7,84 @@ from .utils import *
 TWO_DAYS_AGO = datetime.date.today()-datetime.timedelta(2)
 
 def djinni_scraping(request):
-    city = City.objects.get(name='Киев')
-    specialty = Specialty.objects.get(name='Python')
-    site = Site.objects.get(name='Djinni.co')
+    city = City.objects.get_or_create(name='Киев')
+    specialty = Specialty.objects.get_or_create(name='Python')
+    site = Site.objects.get_or_create(name='Djinni.co')
     url = Url.objects.get(city=city, specialty=specialty, site=site)
-    vacancy = Vacancy.objects.filter(city=city, specialty=specialty, site=site, 
+    vacancy = Vacancy.objects.filter(city=city, specialty=specialty, site=site,
                                     timestamp__gte=TWO_DAYS_AGO).values('url')
     vacancy_url_list = [i['url'] for i in vacancy]
     jobs = []
     jobs = djinni(url.address)
     # print(str(vacancy_url_list).encode('utf-8'))
-    
     for job in jobs:
         if job['href'] not in vacancy_url_list:
             vacancy = Vacancy(city=city, specialty=specialty, site=site,
-                                title=job['title'] , url=job['href'], 
-                                description=job['descript'], 
+                                title=job['title'] , url=job['href'],
+                                description=job['descript'],
                                 company=job['company'])
             vacancy.save()
-        
-    
     return render(request, 'scraping/home.html', {'jobs': jobs})
 
 def work_scraping(request):
-    city = City.objects.get(name='Киев')
-    specialty = Specialty.objects.get(name='Python')
-    site = Site.objects.get(name='Work.ua')
+    city = City.objects.get_or_create(name='Киев')
+    specialty = Specialty.objects.get_or_create(name='Python')
+    site = Site.objects.get_or_create(name='Work.ua')
     url = Url.objects.get(city=city, specialty=specialty, site=site)
-    vacancy = Vacancy.objects.filter(city=city, specialty=specialty, site=site, 
+    vacancy = Vacancy.objects.filter(city=city, specialty=specialty, site=site,
                                     timestamp__gte=TWO_DAYS_AGO).values('url')
     vacancy_url_list = [i['url'] for i in vacancy]
     jobs = []
     jobs = work(url.address)
     # print(city, specialty, site, url.address)
-    
+
     for job in jobs:
         if job['href'] not in vacancy_url_list:
             vacancy = Vacancy(city=city, specialty=specialty, site=site,
-                                title=job['title'] , url=job['href'], 
-                                description=job['descript'], 
+                                title=job['title'] , url=job['href'],
+                                description=job['descript'],
                                 company=job['company'])
             vacancy.save()
-        
-    
     return render(request, 'scraping/home.html', {'jobs': jobs})
-    
+
+
 def rabota_scraping(request):
-    city = City.objects.get(name='Киев')
-    specialty = Specialty.objects.get(name='Python')
-    site = Site.objects.get(name='Rabota.ua')
+    city = City.objects.get_or_create(name='Киев')
+    specialty = Specialty.objects.get_or_create(name='Python')
+    site = Site.objects.get_or_create(name='Rabota.ua')
     url = Url.objects.get(city=city, specialty=specialty, site=site)
-    vacancy = Vacancy.objects.filter(city=city, specialty=specialty, site=site, 
+    vacancy = Vacancy.objects.filter(city=city, specialty=specialty, site=site,
                                     timestamp__gte=TWO_DAYS_AGO).values('url')
     vacancy_url_list = [i['url'] for i in vacancy]
     jobs = []
     jobs = rabota(url.address)
     # print(city, specialty, site, url.address)
-    
     for job in jobs:
         if job['href'] not in vacancy_url_list:
             vacancy = Vacancy(city=city, specialty=specialty, site=site,
-                                title=job['title'] , url=job['href'], 
-                                description=job['descript'], 
+                                title=job['title'] , url=job['href'],
+                                description=job['descript'],
                                 company=job['company'])
             vacancy.save()
-        
-    
     return render(request, 'scraping/home.html', {'jobs': jobs})
-    
+
+
 def dou_scraping(request):
-    city = City.objects.get(name='Киев')
-    specialty = Specialty.objects.get(name='Python')
-    site = Site.objects.get(name='Dou.ua')
+    city = City.objects.get_or_create(name='Киев')
+    specialty = Specialty.objects.get_or_create(name='Python')
+    site = Site.objects.get_or_create(name='Dou.ua')
     url = Url.objects.get(city=city, specialty=specialty, site=site)
-    vacancy = Vacancy.objects.filter(city=city, specialty=specialty, site=site, 
+    vacancy = Vacancy.objects.filter(city=city, specialty=specialty, site=site,
                                     timestamp__gte=TWO_DAYS_AGO).values('url')
     vacancy_url_list = [i['url'] for i in vacancy]
     jobs = []
     jobs = dou(url.address)
     # print(city, specialty, site, url.address)
-    
     for job in jobs:
         if job['href'] not in vacancy_url_list:
             vacancy = Vacancy(city=city, specialty=specialty, site=site,
-                                title=job['title'] , url=job['href'], 
-                                description=job['descript'], 
+                                title=job['title'] , url=job['href'],
+                                description=job['descript'],
                                 company=job['company'])
             vacancy.save()
-        
-    
     return render(request, 'scraping/home.html', {'jobs': jobs})
