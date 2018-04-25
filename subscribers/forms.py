00c2887ea22022d2f18974ac9895ba252cfa69cf
Oleg Novikov <jf2@ua.fm>
2018-04-25 13:11:32 +0300

    changed clean_password method

--- subscribers/forms.py
+++ subscribers/forms.py
@@ -13,30 +13,31 @@ class SubscriberModelForm(forms.ModelForm):
                            widget=forms.EmailInput(attrs={"class": 'form-control'}))
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(
                                             attrs={"class": 'form-control'}))
-    is_active = forms.BooleanField(label='Получать рассылку?',
-                                required=False, widget=forms.CheckboxInput())
-    
+
     class Meta(object):
         model = Subscriber
-        fields = ('name', 'city', 'specialty', 'email', 'password', 'is_active')
+        fields = ('name', 'email', 'password', 'city', 'specialty')
+        exclude = ('is_active',)
         
 
 class LogInForm(forms.Form):
     
     email = forms.EmailField(label='Email', required=True,
                            widget=forms.EmailInput(attrs={"class": 'form-control'}))
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(
-                                            attrs={"class": 'form-control'}))
+    password = forms.CharField(label='Пароль', required=True, 
+                    widget=forms.PasswordInput(attrs={"class": 'form-control'}))
                                             
     def clean_password(self, *args, **kwargs):
         email = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
-        print('fgh')
+        
         if password and email:
-            qs = Subscriber.objects.filter(email=email, password=password).first()
-            print(qs)
+            qs = Subscriber.objects.filter(email=email).first()
             if qs == None:
-                raise forms.ValidationError('Пользователя с таким имейлом и паролем не существует')
+                raise forms.ValidationError("""Пользователя с таким имейлом 
+                                            не существует! Зарегистрируйтесь""")
+            elif password != qs.password:
+                raise forms.ValidationError("""Вы ввели неверный пароль!""")
         return email
         
     
