5d6e6c7886d813f1a7636de6ae10e61ec4f4c201
Oleg Novikov <jf2@ua.fm>
2018-04-25 13:17:23 +0300

    improve vacancy_list function

--- scraping/views.py
+++ scraping/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 import datetime
 
 from .models import *
@@ -12,20 +12,22 @@ def home(request):
 
 
 def vacancy_list(request):
+    form = FindVacancyForm
     if request.GET:
-        city = int(request.GET.get('city'))
-        specialty = int(request.GET.get('specialty'))
-        qs = ''
-        if city and specialty:
-            qs = Vacancy.objects.filter(city=city, specialty=specialty)
-            city = City.objects.get(id=city)
-            specialty = Specialty.objects.get(id=specialty)
-        
-        form = FindVacancyForm
-        return render(request, 'scraping/list.html', {'form': form, 
-                                    'object_list': qs, 'city_name': city.name, 'specialty_name': specialty.name})
-    else:
-        form = FindVacancyForm
+        try:
+            city_id = int(request.GET.get('city'))
+            specialty_id = int(request.GET.get('specialty'))
+        except ValueError:
+            raise Http404("Страница не найдена")
+        context = {}
+        qs = Vacancy.objects.filter(city=city_id, specialty=specialty_id)
+        if qs:
+            context = {'form': form, 'object_list': qs, 
+                        'city_name': qs[0].city.name, 
+                        'specialty_name': qs[0].specialty.name}
+                                
+        return render(request, 'scraping/list.html', context)
+
     return render(request, 'scraping/list.html', {'form': form,})
 
         
