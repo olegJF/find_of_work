466250688c7a6229a9de3a82ebaa2d1fc2e4b9d2
Oleg Novikov <jf2@ua.fm>
2018-04-25 16:27:39 +0300

    uninstall sendgrid

--- send_emails.py
+++ send_emails.py
@@ -3,7 +3,8 @@ import smtplib
 import psycopg2
 import logging
 import datetime
-import sendgrid
+
+import requests
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -14,17 +15,18 @@ path = ''.join([dir, '\\vacancy\\settings\\secret.py'])
 if os.path.exists(path):
     # print('File exists')
     from vacancy.settings.secret import (DB_PASSWORD, DB_HOST, EMAIL,
-                                        DB_NAME, DB_USER, PASSWORD, SENDGRID_KEY
+                                        DB_NAME, DB_USER, PASSWORD, MAILGUN_KEY
                                         )
 else:
     DB_PASSWORD = os.environ.get('DB_PASSWORD')
     PASSWORD = os.environ.get('PASSWORD')
-    EMAIL = os.environ.get('DB_NAME')
+    EMAIL = os.environ.get('EMAIL')
     DB_HOST = os.environ.get('DB_HOST')
     DB_NAME = os.environ.get('DB_NAME')
     DB_USER = os.environ.get('DB_USER')
-    SENDGRID_KEY = os.environ.get('SENDGRID_KEY')
+    MAILGUN_KEY = os.environ.get('MAILGUN_KEY')
 
+ADDRESS = "https://api.mailgun.net/v3/sandbox62c562b960c34f34bec9fd5a60c087b2.mailgun.org/messages"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = EMAIL
 EMAIL_HOST_PASSWORD = PASSWORD
@@ -99,21 +101,24 @@ else:
                 # s.login(EMAIL, PASSWORD)
                 # s.sendmail(FROM_EMAIL, email, msg.as_string())
                 # s.quit()
-                Subject = 'Рассылка списка вакансий за  {}'.format(today)
-                sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_KEY)
-                data = {
-                        "personalizations": [{
-                            "to": [{"email": email }],
-                            "subject": Subject
-                            }],
-                        "from": {"email": FROM_EMAIL },
-                        "content": [{"type": "text/html",
-                            "value": template }]
-                    }
-                response = sg.client.mail.send.post(request_body=data)
+                Subject = 'Список вакансий за  {}'.format(today)
+                # sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_KEY)
+                # data = {
+                #         "personalizations": [{
+                #             "to": [{"email": email }],
+                #             "subject": Subject
+                #             }],
+                #         "from": {"email": FROM_EMAIL },
+                #         "content": [{"type": "text/html",
+                #             "value": template }]
+                #     }
+                # response = sg.client.mail.send.post(request_body=data)
                 # print(response.status_code)
                 # print(response.body)
                 # print(response.headers)
+                requests.post( ADDRESS, auth=("api", MAILGUN_KEY), 
+                                data={"from": EMAIL, "to": email,
+                                "subject": Subject, "html": template})
 
         # print(str(template).encode('utf-8'))
     # return HttpResponse( '<h1>God!</h1>')
--- vacancy/settings/__init__.py
+++ vacancy/settings/__init__.py
@@ -1,8 +1,9 @@
 from .base import *
 
-from .production import *
+# from .production import *
 
 try:
    from .local import *
 except:
+   print('Some problem with local.py')
    pass
