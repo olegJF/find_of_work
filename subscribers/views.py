8bbe350c25829d21c056e6a017a12fa595290c57
Oleg Novikov <jf2@ua.fm>
2018-04-25 13:15:31 +0300

    added messages

--- subscribers/views.py
+++ subscribers/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.urlresolvers import reverse_lazy
+from django.contrib import messages
 
-from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, FormView, UpdateView, DeleteView
 from .models import Subscriber
 from .forms import SubscriberModelForm, LogInForm, SubscriberHiddenEmailForm
 
-class SubscriberCreate(CreateView):
-    model = Subscriber
+class SubscriberCreate(FormView):
     form_class = SubscriberModelForm
     template_name = 'subscribers/create.html'
     success_url = reverse_lazy('home')
     
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        if form.is_valid():
+            messages.success(request, 'Данные успешно сохранены!')
+            
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+            
+    # def post(self, request, *args, **kwargs):
+    #     messages.success(request, 'Данные успешно сохранены')
+    #     return request
+    # 
 
-class SubscriberUpdate(UpdateView):
-    model = Subscriber
-    form_class = SubscriberModelForm
-    template_name = 'subscribers/create.html'
-    success_url = reverse_lazy('home')
+    
+    
+    
+    
     
 def login_subscriber(request):
     if request.method == "GET":
@@ -35,7 +48,7 @@ def update_subscriber(request):
     if request.method == "GET" and request.session.get('email', False):
         email = request.session.get('email')
         qs = Subscriber.objects.filter(email=email).first()
-        print(qs)
+        
         form = SubscriberHiddenEmailForm(initial={'name': qs.name, 'city': qs.city, 
                         'specialty': qs.specialty, 'email': qs.email, 
                         'password': qs.password, 'is_active': qs.is_active})
@@ -46,7 +59,9 @@ def update_subscriber(request):
         form = SubscriberHiddenEmailForm(request.POST or None, instance=user)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Данные успешно сохранены')
             return redirect('home')
+        messages.error(request, 'Необходимо корректно заполнить  все поля формы')
         return render(request, 'subscribers/update.html', {'form': form})
             
     else:
