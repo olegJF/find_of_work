8056b9e903153a67037c7d06d2ebc12b6a86655e
Oleg Novikov <jf2@ua.fm>
2018-04-25 13:13:31 +0300

    changed display of subscriber's list in admin

--- subscribers/admin.py
+++ subscribers/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Subscriber
 
-admin.site.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+
+    class Meta:
+        model = Subscriber
+    list_display = ('name', 'email', 'specialty', 'city', 'is_active')
+    list_editable = ['is_active']
+
+admin.site.register(Subscriber, SubscriberAdmin)
